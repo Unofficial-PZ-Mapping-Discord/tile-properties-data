703daf6d56e4fd386060d0c8a6f95f53ca983afd
SimKDT <simon.cadet79@hotmail.fr>
2026-06-09 01:38:00 +0200

    Fix formatting

--- chores/tile_properties.py
+++ chores/tile_properties.py
@@ -9,23 +9,25 @@ OUTPUT = Path("out/tile_properties.json")
 
 # load properties from java parser
 with open(JAVA_INPUT) as f:
-    property = json.load(f)
+    data = json.load(f)
 
 
 # load dataset
 with open(DATA_INPUT) as f:
-    data = yaml.safe_load(f)
+    property = yaml.safe_load(f)['objects']
 
 # check provided data is not providing for a non-existing tile property
-for name in data["objects"].keys():
+for name in data.keys():
     if name not in property:
-        print(f"Warning: provided data for non-existing or deprecated tile property '{name}'")
+        property[name] = data[name]
+    else:
+        property[name]['field'] = data[name]['field']
 
 # copy #desc to description
 for prop in property.values():
     if "#desc" in prop:
         ref = prop["#desc"]
-        if ref not in property:
+        if ref not in data:
             print(f"Warning: description reference '{ref}' not found for property '{prop['field']}'")
             continue
         prop["description"] = property[ref]["description"]
